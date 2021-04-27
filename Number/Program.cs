1917541ac13c60306232c64a709f6494939675c4
Sixtten610 <60339623+Sixtten610@users.noreply.github.com>
2021-04-27 12:33:41 +0200

    yes 🚄

---
+++ Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            while (true)
+            {
+                Console.WriteLine("Write number:");
+                string s = Console.ReadLine();
+
+                // försöker konvertera stringen från consolreadline till en int;    
+                bool convert = int.TryParse(s, out number);
+                // om convert skriv success meddelande
+                if (convert)
+                {
+                    System.Console.WriteLine("nice " + number + " is a number!");
+                }
+                // om inte ge instruktioner hur man går tillväga
+                else
+                {
+                    System.Console.WriteLine(s + " does not work, try a number i.e: '2'");
+                }
+
+            }
+        }
+    }
+}
